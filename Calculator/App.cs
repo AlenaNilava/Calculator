b7d15f7fc47644acc0ab993743cc7a63082c09af
Alena Nilava <44751670+AlenaNilava@users.noreply.github.com>
2018-11-04 20:51:43 +0300

    Initial commit

---
+++ Calculator/App.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+    class App
+    {
+        static string anyLetter = "[a-zA-Z]";
+
+		static void Main(string[] args)
+		{
+			//Welcome to Calculator
+			Console.WriteLine("Hello user! Welcome to calculator.");
+			Start:
+			Console.WriteLine("Enter your operation (for instance: 2 + 2):");
+
+			//Read user's input
+			string userInput = Console.ReadLine();
+
+			//Validate user's input
+			Error:
+			while (string.IsNullOrEmpty(userInput) || Regex.IsMatch(userInput, anyLetter)
+				|| string.IsNullOrWhiteSpace(userInput)
+				|| Operations.getOperand(userInput) == ' ')
+			{
+				Console.WriteLine();
+
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Input is incorrect, enter 2 numbers with operations: " +
+					"\nMultiplication: *\nAddition: +\nDivision: /\nSubstraction: -");
+
+				Console.ResetColor();
+
+				Console.WriteLine("Enter your operation (for instance: 2 + 2):");
+				userInput = Console.ReadLine();
+			}
+
+			//Get user's operation
+			char operand = Operations.getOperand(userInput);
+			//Get user's numbers
+			string[] numbers = Operations.getNumbers(userInput, operand);
+			//Get user's first number
+			decimal number1 = Operations.getNumber(numbers[0]);
+			//Get user's Second number
+			decimal number2 = Operations.getNumber(numbers[1]);
+
+			//Check that we do not divide by 0
+			if (number2 == 0)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("You cannot divide on zero");
+				userInput = null;
+				goto Error;
+			}
+
+			//Do calculation with numbers
+			if (number1 != decimal.MinValue && number2 != decimal.MinValue)
+			{
+				Console.WriteLine("Answer is: {0}", Operations.Calc(number1, number2, operand));
+				Console.WriteLine();
+				Console.WriteLine("If you want to continue calculations, press Enter");
+				if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+				{
+					goto Start;
+				}
+
+			}
+			else
+			{
+				userInput = null;
+				goto Error;
+			}
+			Console.WriteLine("Thank you for using calculator. Bye!");
+		}
+    }
+}
+
---
+++ Calculator/Operations.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calculator
+{
+	class Operations
+	{
+		static string anyNumber = "^\\d+(.\\d+){0,1}$";
+
+		//Get user's numbers
+		public static string[] getNumbers(string userInput, char operand)
+		{
+			return Array.ConvertAll(userInput.Replace(',', '.').Split(operand), p => p.Trim());
+		}
+
+		//Do calculation with user's numbers and choose operation
+		public static decimal Calc(decimal number1, decimal number2, char operand)
+		{
+			switch (operand)
+			{
+				case '*':
+					return number1 * number2;
+				case '/':
+					return number1 / number2;
+				case '+':
+					return number1 + number2;
+				case '-':
+					return number1 - number2;
+			}
+			return decimal.MinValue;
+		}
+
+		//Get valid user's number
+		public static decimal getNumber(string userInput)
+		{
+			if (Regex.IsMatch(userInput, anyNumber))
+			{
+				try
+				{
+					return decimal.Parse(userInput, CultureInfo.InvariantCulture);
+				}
+				catch
+				{
+					return decimal.MinValue;
+				}
+			}
+			return decimal.MinValue;
+		}
+
+		//Get an operand from user's input
+		public static char getOperand(string userInput)
+		{
+			char[] operators = { '+', '-', '/', '*' };
+
+			int count = 0;
+			char oper = ' ';
+
+			foreach (char operand in operators)
+			{
+				if (userInput.IndexOf(operand) != -1)
+				{
+					oper = operand;
+					count++;
+				}
+			}
+
+			if (count != 1)
+			{
+				return ' ';
+			}
+
+			return oper;
+		}
+	}
+}
